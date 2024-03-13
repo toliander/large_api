2611bc4f3fa8e0ff309161345ba42b125fb5f36d
Sanechek <bogdanovalexandr1996@gmail.com>
2024-03-13 13:57:15 +0300

    Second_task

--- 1234.py
+++ 1234.py
@@ -11,28 +11,40 @@ class ApiWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Большая задача по api - 1')
-        self.kartinka.setPixmap(QtGui.QPixmap('image.png'))
+        self.setWindowTitle('Большая задача по api - 2')
+        # Моя обитель находится по следующим координатам
+        self.coords = ('55.831216', '37.526286')[::-1]
+        # максимальный размер 650х450
+        self.size = ('650', '450')
+        # Изменяется в пределах 0 - 21
+        self.scale = 15
+        self.get_map()
+
+    def get_map(self):
+        request = f"http://static-maps.yandex.ru/1.x/?ll=" \
+                  f"{','.join(self.coords)}&size={','.join(self.size)}&z={self.scale}&l=map"
+        response = requests.get(request)
+        if response.status_code == 200:
+            with open('image.png', 'wb') as file:
+                file.write(response.content)
+            self.kartinka.setPixmap(QtGui.QPixmap('image.png'))
+        else:
+            self.kartinka.setText('Ошибка получения изображения')
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        page_up = 16777238
+        page_down = 16777239
+        if key == page_up:
+            if self.scale < 21:
+                self.scale += 1
+        elif key == page_down:
+            if self.scale > 0:
+                self.scale -= 1
+        self.get_map()
 
 
-# Моя обитель находится по следующим координатам
-coordinates = map(str, (55.831216, 37.526286)[::-1])
-# максимальный размер 650х450
-size = map(str, (650, 450))
-# Изменяется в пределах 0 - 21
-scale = 15
-
-apikey = "40d1649f-0493-4b70-98ba-98533de7710b"
-request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coordinates)}&size={','.join(size)}&z={scale}&l=map"
-
-response = requests.get(request)
-
-if response.status_code == 200:
-    with open('image.png', 'wb') as file:
-        file.write(response.content)
-    app = QApplication(sys.argv)
-    ex = ApiWindow()
-    ex.show()
-    sys.exit(app.exec_())
-else:
-    print('Ошибка при получении изображения.')
+app = QApplication(sys.argv)
+ex = ApiWindow()
+ex.show()
+sys.exit(app.exec_())
